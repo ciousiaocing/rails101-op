96bb0e0698e7309e64a6a44e7619da20406138b8
ciou <yyouko22@gmail.com>
2016-09-29 01:24:54 +0800

    ch4 step6-9

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,12 @@
 class PostsController < ApplicationController
+
+  before_action :find_group
+
   def new
-    @group = Group.find(params[:group_id])
     @post = @group.posts.new
   end
 
   def create
-    @group = Group.find(params[:group_id])
     @post = @group.posts.build(post_params)
 
     if @post.save
@@ -15,7 +16,32 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = @group.posts.find(params[:id])
+  end
+
+  def update
+    @post = @group.posts.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to group_path(@group), notice: "文章修改成功"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = @group.posts.find(params[:id])
+
+    @post.destroy
+    redirect_to group_path(@group), alert: "文章已刪除"
+  end
+
   private
+
+  def find_parmas
+    @group = Group.find(params[:group_id])
+  end
 
   def post_params
     params.require(:post).permit(:content)
