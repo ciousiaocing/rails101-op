02f1a6ebbfa19a0349a1a78459ce10f30f901db5
ciou <yyouko22@gmail.com>
2016-09-30 00:42:36 +0800

    ch6 (2)

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,7 +11,7 @@ class GroupsController < ApplicationController
   end
 
   def create
-    @group = Group.new(group_params)
+    @group = current_user.groups.new(group_params)
 
     if @group.save
       redirect_to groups_path
@@ -26,11 +26,11 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
+    @group = current_user.groups.find(params[:id])
   end
 
   def update
-    @group = Group.find(params[:id])
+    @group = current_user.groups.find(params[:id])
 
     if @group.update(group_params)
       redirect_to groups_path, notice: "修改討論版成功"
@@ -40,7 +40,7 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-    @group = Group.find(params[:id])
+    @group = current_user.groups.find(params[:id])
     @group.destroy
     redirect_to groups_path, alert: "討論版已刪除"
   end
